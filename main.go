6f7b2d6373cf2bdd1e089091ab0f1ae41d9da41c
YuryBatov <batov1998@list.ru>
2018-10-08 17:02:11 +0300

    gh

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import "fmt"
+import "io/ioutil"
 
 func main() {
-
-	fmt.Println("Hello G2424o")
+	data, err := ioutil.ReadFile("test.txt")
+	if err != nil {
+		fmt.Println("Ошибка чтения файла")
+	}
+	int(data)
+	for i=1;i<n;i++ {
+		for j=i;j>0 && data[j-1]>data[j];j-- {
+							a = data[i]
+				data[i]=data[j]
+				data[j]=a	
+			}
+	}
+	fmt.Print(string(data))
 }
