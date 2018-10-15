77ad89c82a8ab68dd9325ed7e16aa81eb0ca574c
YuryBatov <batov1998@list.ru>
2018-10-15 23:00:45 +0300

    Почти доделал

--- Work2/Work2.go
+++ Work2/Work2.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 
-	"./countword"
 	"./readfile"
 )
 
@@ -15,43 +14,61 @@ type files struct {
 	quantity int
 }
 
-/**type revers_index struct{
-	name_word string
-	name_files []string
-}*/
-
-func sort(data1 []files, n int) []files {
-	for i := 1; i < n; i++ {
-		for j := i; j > 0 && data1[j-1].quantity < data1[j].quantity; j-- {
-			data1[j-1], data1[j] = data1[j], data1[j-1]
+func sort(m map[string][]files, str string) map[string][]files {
+	for i := range m[str] {
+		for j := i; j > 0 && m[str][j-1].quantity < m[str][j].quantity; j-- {
+			m[str][j-1], m[str][j] = m[str][j], m[str][j-1]
 		}
 	}
-	return data1
+	return m
+}
+func revers_ind(str []string, files_name string, m map[string][]files) map[string][]files {
+	//var data []files
+	var data1 files
+	data1.name = files_name
+	data1.quantity = 1
+	//var k int
+	for i := range str {
+		k := 0
+		if m[str[i]] != nil {
+			for j := range m[str[i]] {
+				if m[str[i]][j].name == files_name {
+					m[str[i]][j].quantity++
+					k++
+				}
+			}
+			if k == 0 {
+				m[str[i]] = append(m[str[i]], data1)
+			}
+		}
+		if m[str[i]] == nil {
+			m[str[i]] = append(m[str[i]], data1)
+		}
+		k = 0
+		m = sort(m, str[i])
+	}
+
+	return m
 }
 
 func main() {
+	m := make(map[string][]files)
 	var word string
 	var words []string
-	var data files
-	var data1 []files
 	files_name := make([]string, 0, 10)
 	files_name = os.Args[1:]
-	fmt.Printf("%s", files_name)
 	fmt.Printf("\n%s", "Введите слово: ")
+	for i := range files_name {
+		str1 := readfile.Readfile(files_name[i])
+		m = revers_ind(str1, files_name[i], m)
+	}
 	buf := bufio.NewScanner(os.Stdin)
 	buf.Scan()
 	word = buf.Text()
 	words = strings.Split(word, " ")
-	for i := range files_name {
-		data.name = files_name[i]
-		str1 := readfile.Readfile(files_name[i])
-		data.quantity = countword.Countword(str1, words)
-		data1 = append(data1, data)
-	}
-	data1 = sort(data1, len(files_name))
-	for i := range files_name {
-		if data1[i].quantity != 0 {
-			fmt.Printf("\n%s; совпадений - %d", data1[i].name, data1[i].quantity)
+	for i := range words {
+		for j := range m[words[i]] {
+			fmt.Printf("%s: совпадений %d\n", m[words[i]][j].name, m[words[i]][j].quantity)
 		}
 	}
 }
