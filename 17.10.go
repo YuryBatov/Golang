d334c0acfdceaf4869104114a226c6b95b0f2dae
YuryBatov <batov1998@list.ru>
2018-10-23 20:38:08 +0300

    Исправления

---
+++ 17.10.go
@@ -0,0 +1,6 @@
+// TrimSpace удаляет указанный символ справа слево от исходного(num, err = strconv.Atoi(string.TrimSpace(i)))
+// таймер <- times.C функция AfterFunc , вызывает функцию через какое то время?
+// contex нужен для коректного прекращения работы канала
+//WaitGroup (wg := &sync.WaitGroup{} )
+// Mutex
+// atomic
